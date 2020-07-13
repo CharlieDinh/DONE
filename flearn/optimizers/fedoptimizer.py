d265d42d1904c427b7e1930c8d30f8b7bb60e8b5
CharlieDinh <CharlieSex@156>
2020-07-13 14:41:22 +1000

    Initial commit

---
+++ flearn/optimizers/fedoptimizer.py
@@ -0,0 +1,195 @@
+from torch.optim import Optimizer
+import numpy as np
+import torch
+
+class MySGD(Optimizer):
+    def __init__(self, params, lr):
+        defaults = dict(lr=lr)
+        super(MySGD, self).__init__(params, defaults)
+
+    def step(self, closure=None, hyper_learning_rate = 0):
+        loss = None
+        if closure is not None:
+            loss = closure
+
+        for group in self.param_groups:
+            # print(group)
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if(hyper_learning_rate != 0):
+                    p.data.add_(-hyper_learning_rate, d_p)
+                else:     
+                    p.data.add_(-group['lr'], d_p)
+        return loss
+
+
+class FEDLOptimizer(Optimizer):
+    def __init__(self, params, lr = 0.01, hyper_lr = 0.01,  L = 0.1):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr,hyper_lr= hyper_lr, L = L)
+        super(FEDLOptimizer, self).__init__(params, defaults)
+
+    def step(self, server_grads, pre_grads, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure
+        for group in self.param_groups:
+            for p, server_grad, pre_grad in zip(group['params'],server_grads, pre_grads):
+                if(server_grad.grad != None and pre_grad.grad != None):
+                    p.data = p.data - group['lr'] * (p.grad.data + group['hyper_lr'] * server_grad.grad.data - pre_grad.grad.data)
+                else:
+                     p.data = p.data - group['lr'] * p.grad.data
+        return loss
+
+class pFedMeOptimizer(Optimizer):
+    def __init__(self, params, lr=0.01, L=0.1 , mu = 0.001):
+        #self.local_weight_updated = local_weight # w_i,K
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr, L=L, mu = mu)
+        super(pFedMeOptimizer, self).__init__(params, defaults)
+    
+    def step(self, local_weight_updated, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure
+        weight_update = local_weight_updated.copy()
+        for group in self.param_groups:
+            for p, localweight in zip( group['params'], weight_update):
+                p.data = p.data - group['lr'] * (p.grad.data + group['L'] * (p.data - localweight.data) + group['mu']*p.data)
+        return  group['params'], loss
+    
+    def update_param(self, local_weight_updated, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure
+        weight_update = local_weight_updated.copy()
+        for group in self.param_groups:
+            for p, localweight in zip( group['params'], weight_update):
+                p.data = localweight.data
+        #return  p.data
+        return  group['params']
+
+
+class Neumann(Optimizer):
+    """
+    Documentation about the algorithm
+    """
+
+    def __init__(self, params , lr=1e-3, eps = 1e-8, alpha = 1e-7, beta = 1e-5, gamma = 0.9, momentum = 1, sgd_steps = 5, K = 10 ):
+        
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 1 >= momentum:
+            raise ValueError("Invalid momentum value: {}".format(eps))
+        
+
+        self.iter = 0
+        # self.sgd = SGD(params, lr=lr, momentum=0.9)
+
+        param_count = np.sum([np.prod(p.size()) for p in params]) # got from MNIST-GAN
+
+        defaults = dict(lr=lr, eps=eps, alpha=alpha,
+                    beta=beta*param_count, gamma=gamma,
+                    sgd_steps=sgd_steps, momentum=momentum, K=K
+                    )
+
+        super(Neumann, self).__init__(params, defaults)
+
+
+    def step(self, closure=None):
+        """
+        Performs a single optimization step.
+        
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        self.iter += 1
+
+
+        loss = None
+        if closure is not None: #checkout what's the deal with this. present in multiple pytorch optimizers
+            loss = closure()
+
+        for group in self.param_groups:
+
+            sgd_steps = group['sgd_steps']
+
+            alpha = group['alpha']
+            beta = group['beta']
+            gamma = group['gamma']
+            K = group['K']
+            momentum = group['momentum']
+            mu = momentum*(1 - (1/(1+self.iter)))
+            
+            if mu >= 0.9:
+                mu = 0.9
+            elif mu <= 0.5:
+                mu = 0.5
+
+
+            eta = group['lr']/self.iter ## update with time ## changed
+            # print("here")
+            
+            
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data 
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['m'] = torch.zeros_like(p.data).float()
+                    state['d'] = torch.zeros_like(p.data).float()
+                    state['moving_avg'] = p.data
+
+
+                if self.iter <= sgd_steps:
+                
+                    p.data.add_(-group['lr'], grad)
+                    # self.sgd.step()
+                    continue
+
+                state['step'] += 1
+
+
+                # Reset neumann iterate 
+                if self.iter%K == 0:
+                    state['m'] = grad.mul(-eta)
+                    ## changed                  
+
+                else:   
+                    ## Compute update d_t
+                    diff = p.data.sub(state['moving_avg'])
+                    # # print(diff)
+                    #diff_norm = p.data.sub(state['moving_avg']).norm()
+                    #if np.count_nonzero(diff) and diff_norm > 0:
+                    #    state['d'] = grad.add( (( (diff_norm.pow(2)).mul(alpha) ).sub( (diff_norm.pow(-2)).mul(beta) )).mul( diff.div(diff_norm)) )
+                    #else:
+                    #    state['d'].add_(grad)
+                    state['d'] = grad
+
+                    ## Update Neumann iterate
+                    (state['m'].mul_(mu)).sub_( state['d'].mul(eta) )
+
+                    ## Update Weights
+                    p.data.add_((state['m'].mul(mu)).sub( state['d'].mul(eta)))
+
+                    ## Update Moving Average
+                    #state['moving_avg'] = p.data.add( (state['moving_avg'].sub(p.data)).mul(gamma) )
+
+                # print(p.data)
+
+        ## changed
+        if self.iter%K == 0:
+            group['K'] = group['K']*2
+        
+        # return loss
---
+++ main_femnist.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import importlib
+import random
+import os
+from flearn.servers.serveravg import FedAvg
+from flearn.servers.serverfedl import FEDL
+from flearn.servers.serverneumann import FedNeumann
+from flearn.trainmodel.models import *
+from utils.plot_utils import *
+import torch
+torch.manual_seed(0)
+    
+  
+numusers = 10
+num_glob_iters = 600
+dataset = "Femnist"
+local_ep = 20
+learning_rate = 0.003
+hyper_learning_rate =  0
+batch_size = 32
+algorithm = "Neumann"
+optimizer = "SGD"
+i = 0
+model =  "mclr"
+model = Mclr_Logistic(input_dim = 784, output_dim = 62), model
+L = 0
+
+if(0):
+    if(algorithm == "FedAvg"):
+        server = FedAvg(dataset, algorithm, model, batch_size, learning_rate, hyper_learning_rate, L, num_glob_iters, local_ep, optimizer, numusers, i)
+            
+    if(algorithm == "Neumann"):
+        server = FedNeumann(dataset, algorithm, model, batch_size, learning_rate, hyper_learning_rate, L, num_glob_iters, local_ep, optimizer, numusers, i)
+        
+    server.train()
+    server.test()
+
+    average_data(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L,learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms=algorithm, batch_size=batch_size, dataset=dataset,times = i)
+
+if(1):
+    local_ep = [20,20]
+    L = [0,0]
+    learning_rate = [0.003, 0.003]
+    hyper_learning_rate =  [0, 0, 0]
+    batch_size = [32,32,32]
+    algorithms = ["Neumann","FedAvg"]
+    plot_summary_one_figure(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
