366e1f89bf9e1e74b5bf4edbf371bf604cab5475
CharlieDinh <CharlieSex@156>
2020-07-15 11:04:33 +1000

    Update main mnist for testing

--- main_mnist.py
+++ main_mnist.py
@@ -13,37 +13,30 @@ import torch
 torch.manual_seed(0)
     
   
-numusers = 10
+numedges = 10
 num_glob_iters = 600
 dataset = "Mnist"
-local_ep = 20
+local_epochs = 20
 learning_rate = 0.001
-hyper_learning_rate =  0
+eta =  0
 batch_size = 32
-algorithm = "FedAvg"
+algorithm = "FirstOrder"
 optimizer = "SGD"
 i = 0
 model =  "mclr"
 model = Mclr_Logistic(), model
 L = 0
 
-if(0):
-    if(algorithm == "FedAvg"):
-        server = FedAvg(dataset, algorithm, model, batch_size, learning_rate, hyper_learning_rate, L, num_glob_iters, local_ep, optimizer, numusers, i)
-            
-    if(algorithm == "Neumann"):
-        server = FedNeumann(dataset, algorithm, model, batch_size, learning_rate, hyper_learning_rate, L, num_glob_iters, local_ep, optimizer, numusers, i)
-        
+if(1):
+    server = Server(dataset, algorithm, model, batch_size, learning_rate, eta, L, num_glob_iters, local_epochs, optimizer, numedges, i)
     server.train()
     server.test()
 
-    average_data(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L,learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms=algorithm, batch_size=batch_size, dataset=dataset,times = i)
-
-if(1):
+if(0):
     local_ep = [20,20]
     L = [0,0]
     learning_rate = [0.2, 0.001]
     hyper_learning_rate =  [0, 0, 0]
     batch_size = [32,32,32]
     algorithms = ["Neumann","FedAvg"]
-    plot_summary_one_figure(num_users=numusers, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
+    plot_summary_one_figure(num_users=numedges, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
