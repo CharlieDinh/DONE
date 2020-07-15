6d5eaa2a3570a09829517390baa5acc6a647363a
tuann6 <tuandungnguyen99@gmail.com>
2020-07-15 16:49:36 +1000

    Update 2-nd order rule

--- algorithms/edges/edgeSeOrder.py
+++ algorithms/edges/edgeSeOrder.py
@@ -8,6 +8,8 @@ import json
 from torch.utils.data import DataLoader
 from algorithms.edges.edgebase import Edgebase
 from algorithms.optimizers.optimizer import *
+
+
 # Implementation for FedAvg clients
 
 class edgeSeOrder(Edgebase):
@@ -16,11 +18,11 @@ class edgeSeOrder(Edgebase):
         super().__init__(numeric_id, train_data, test_data, model[0], batch_size, learning_rate, eta, eta0, L,
                          local_epochs)
 
-        if(model[1] == "linear_regression"):
+        if (model[1] == "linear_regression"):
             self.loss = nn.MSELoss()
         else:
             self.loss = nn.NLLLoss()
-            
+
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
 
     def set_grads(self, new_grads):
@@ -32,84 +34,30 @@ class edgeSeOrder(Edgebase):
                 model_grad.data = new_grads[idx]
 
     def train(self, epochs):
-        self.update_dt()
-        # self.model.train()
-        # for epoch in range(1, self.local_epochs + 1):
-        #     self.model.train()
-        #     #loss_per_epoch = 0
-        #     for batch_idx, (X, y) in enumerate(self.trainloader):
-        #         self.optimizer.zero_grad()
-        #         output = self.model(X)
-        #         loss = self.loss(output, y)
-        #         loss.backward()
-        #         self.optimizer.step()
-        # return loss
+        self.model.zero_grad()
 
-    def hessian_vec_prod(self, f, params: list, dt: list):
-        # f.backward(create_graph=True)
-        hess_vec_prods = []
-        for param, d in zip(params, dt):
-            if param.grad is not None:
-                param.grad.zero_()
-            grad, = torch.autograd.grad(f, param, create_graph=True, retain_graph=True)
-            dot = grad.mul(d.clone().detach()).sum()
-            if param.grad is not None:
-                param.grad.zero_()
-            g2, = torch.autograd.grad(dot, param, retain_graph=True)
-            hess_vec_prods.append(g2.data.clone())
-            #param.grad = g2.data.clone()
-        return hess_vec_prods
-
-    def update_dt(self):
-        self.model.train()
+        # Sample a mini-batch (D_i)
         (X, y) = self.get_next_train_batch()
-        self.optimizer.zero_grad()
         output = self.model(X)
         loss = self.loss(output, y)
-        grads = torch.autograd.grad(loss, self.model.parameters(), create_graph=True, retain_graph=True)
-        grads = list(grads)
+        loss.backward(create_graph=True)
+        grads = []
 
-        # dt_0
-        for d, grad in zip(self.dt, grads):
-            d.data = grad.data.clone()
+        # Set d^i_0
+        for d, param in zip(self.dt, self.model.parameters()):
+            d.data = - param.grad.data.clone()
+            grads.append(param.grad.data.clone())
 
+        # Richardson iteration
         for i in range(1, self.local_epochs + 1):  # R
             hess_vec_prods = self.hessian_vec_prod(loss, list(self.model.parameters()), self.dt)
             for grad, d, hess_vec in zip(grads, self.dt, hess_vec_prods):
                 d.data = d.data - self.eta * hess_vec - grad.data
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def hessian_vec_prod(self, loss, params, dt):
+        self.model.zero_grad()
+        grads = torch.autograd.grad(loss, params, create_graph=True, retain_graph=True)
+        self.model.zero_grad()
+        hv = torch.autograd.grad(grads, params, dt, only_inputs=True, create_graph=True, retain_graph=True)
+        return hv
 
