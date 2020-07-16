a0ee40cec02ef33b6c1632a6eeafe48b60f69b07
CharlieDinh <CharlieSex@156>
2020-07-16 16:57:18 +1000

    Update parameter for main_mnist

--- main_mnist.py
+++ main_mnist.py
@@ -12,32 +12,30 @@ from utils.plot_utils import *
 import torch
 torch.manual_seed(0)
     
-  
 numedges = 10
-num_glob_iters = 600
+num_glob_iters = 500
 dataset = "Mnist"
-local_epochs = 20
-learning_rate = 0.001
-eta =  0.5
-eta0 = 0.5
-batch_size = 32
-algorithm = "FirstOrder"
 optimizer = "SGD"
-i = 0
 model =  "mclr"
+times = 1
 model = Mclr_Logistic(), model
-L = 0
 
-if(1):
-    server = Server(dataset, algorithm, model, batch_size, learning_rate, eta, eta0, L, num_glob_iters, local_epochs, optimizer, numedges, i)
-    server.train()
-    server.test()
+# defind parameters
+local_epochs = [20]
+learning_rate = [0.01]
+eta =  [0.05]
+eta0 = [0.05]
+batch_size = [16]
+algorithms = ["FirstOrder"]
+L = [0]
 
 if(0):
-    local_ep = [20,20]
-    L = [0,0]
-    learning_rate = [0.2, 0.001]
-    hyper_learning_rate =  [0, 0, 0]
-    batch_size = [32,32,32]
-    algorithms = ["Neumann","FedAvg"]
-    plot_summary_one_figure(num_users=numedges, loc_ep1=local_ep, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, hyper_learning_rate = hyper_learning_rate, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
+    for i in range(len(algorithms)):
+        for time in range(times):
+            server = Server(dataset, algorithms[i], model, batch_size[i], learning_rate[i], eta[i], eta0[i], L[i], num_glob_iters, local_epochs[i], optimizer, numedges, time)
+            server.train()
+            server.test()
+        average_data(num_users=numedges, loc_ep1=local_epochs[i], Numb_Glob_Iters=num_glob_iters, lamb=L[i], learning_rate=learning_rate[i], eta = eta[i], eta0 = eta0[i], algorithms=algorithms[i], batch_size=batch_size[i], dataset=dataset, times = times)
+
+if(1):
+    plot_summary_one_figure(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, eta = eta, eta0 = eta0, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
