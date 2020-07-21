35b74f3e3bb6f0d915e0e5cdf2b810fb85d1c334
tuann6 <tuandungnguyen99@gmail.com>
2020-07-21 10:17:05 +1000

    Armijo search and conjugate gradient

---
+++ algorithms/edges/nn_utils.py
@@ -0,0 +1,87 @@
+import torch
+import numpy as np
+from torch import nn
+
+def l2_norm(model):
+    weights = model
+    if isinstance(model, nn.Module):
+        weights = list(model.parameters())
+    norm = 0.
+    for weight in weights:
+        norm += weight.data.norm() ** 2
+    # assert norm >= 0.
+    return np.sqrt(norm)
+
+
+def dot_product(weights1, weights2):
+    if isinstance(weights1, nn.Module):
+        weights1 = list(weights1.parameters())
+    if isinstance(weights2, nn.Module):
+        weights2 = list(weights2.parameters())
+    dot = 0.
+    for weight1, weight2 in zip(weights1, weights2):
+        dot += weight1.data.mul(weight2.data).sum()
+    return dot
+
+
+def add_params(to, fr, ratio=1., in_place=True):
+    result = []
+    for t, f in zip(to, fr):
+        if in_place:
+            t.data.add_(ratio * f)
+        else:
+            result.append(t.data + ratio * f)
+    if not in_place:
+        return result
+
+
+def conjugate_gradient(loss, model, iters=10):
+    if isinstance(model, nn.Module):
+        model = list(model.parameters())
+    grads = torch.autograd.grad(loss, model, create_graph=True, retain_graph=True)
+
+    p = [torch.zeros_like(grad) for grad in grads]
+    g = grads
+    d = [-grad.clone().detach() for grad in grads]
+
+    for i in range(iters):
+        Hv = torch.autograd.grad(grads, model, d,
+                                 only_inputs=True, create_graph=True, retain_graph=True)
+        old_g_norm = l2_norm(g)
+        alpha = (old_g_norm ** 2) / (dot_product(d, Hv))
+        add_params(p, d, alpha)
+        add_params(g, Hv, alpha)
+        beta = (l2_norm(g) ** 2) / (old_g_norm ** 2)
+        d = add_params([- data.data for data in g], d, beta, in_place=False)
+
+    return p
+
+def armijo_search(model: nn.Module, direction, X, y, loss_func, beta=0.1, initial_lr=1., tau=0.707):
+    """
+    direction: the descent direction in optimization. e.g. for gradient descent, direction
+               is the nagative of the gradient evaluated at the current weights.
+    loss_func: a callable loss function
+    """
+    initial_f = loss_func(model(X), y)
+    grads = torch.autograd.grad(initial_f, list(model.parameters()), create_graph=True,
+                                retain_graph=True, only_inputs=True)
+    dot = dot_product(grads, direction)
+    lr = initial_lr
+
+    add_params(model.parameters(), direction, lr)
+    while loss_func(model(X), y).data > initial_f + lr * beta * dot:
+        add_params(model.parameters(), direction, (tau - 1) * lr)
+        lr = tau * lr
+
+    # Recover the old weights
+    add_params(model.parameters(), direction, -tau)
+    return lr
+
+def hessian_vec_prod(loss, params, dt):
+    """
+    loss is dependent on params
+    dt is the vector to multiply with the Hessian
+    """
+    grads = torch.autograd.grad(loss, params, create_graph=True, retain_graph=True)
+    hv = torch.autograd.grad(grads, params, dt, only_inputs=True, create_graph=True, retain_graph=True)
+    return hv
