980b9486fdfe64c6e7742192363e72d50a6c8674
tuann6 <tuandungnguyen99@gmail.com>
2020-07-22 16:06:08 +1000

    Add regularized loss

--- algorithms/edges/edgebase.py
+++ algorithms/edges/edgebase.py
@@ -7,18 +7,21 @@ from torch.utils.data import DataLoader
 import numpy as np
 import copy
 
+
 class Edgebase:
     """
     Base class for edges in distributed learning.
     """
-    def __init__(self, id, train_data, test_data, model, batch_size = 0, learning_rate = 0, eta=0, eta0 = 0, L = 0, local_epochs = 0):
+
+    def __init__(self, id, train_data, test_data, model, batch_size=0, learning_rate=0, eta=0, eta0=0, L=0,
+                 local_epochs=0):
         # from fedprox
         self.model = copy.deepcopy(model)
         self.id = id  # integer
         self.train_samples = len(train_data)
         self.test_samples = len(test_data)
-        
-        if(batch_size == 0):
+
+        if (batch_size == 0):
             self.batch_size = len(train_data)
         else:
             self.batch_size = batch_size
@@ -29,7 +32,7 @@ class Edgebase:
         self.L = L
         self.local_epochs = local_epochs
         self.trainloader = DataLoader(train_data, self.batch_size)
-        self.testloader =  DataLoader(test_data, self.batch_size)
+        self.testloader = DataLoader(test_data, self.batch_size)
         self.testloaderfull = DataLoader(test_data, self.test_samples)
         self.trainloaderfull = DataLoader(train_data, self.train_samples)
         self.iter_trainloader = iter(self.trainloader)
@@ -38,17 +41,17 @@ class Edgebase:
         # those parameters are for FEDL.
         self.local_optimal = copy.deepcopy(list(self.model.parameters()))
         self.dt = copy.deepcopy(list(self.model.parameters()))
-        self.server_grad    = copy.deepcopy(list(self.model.parameters()))
+        self.server_grad = copy.deepcopy(list(self.model.parameters()))
         self.pre_local_grad = copy.deepcopy(list(self.model.parameters()))
 
     def set_parameters(self, model):
         for old_param, new_param in zip(self.model.parameters(), model.parameters()):
             old_param.data = new_param.data.clone()
 
-    #def set_server_parameters(self, model):
+    # def set_server_parameters(self, model):
     #    for new_param, server_param in zip(model.parameters(), self.server_parameters):
     #        server_param.data = new_param.data.clone()
-            
+
     def get_parameters(self):
         for param in self.model.parameters():
             param.detach()
@@ -62,21 +65,21 @@ class Edgebase:
     def clone_model_paramenter(self, param, clone_param):
         for param, clone_param in zip(param, clone_param):
             clone_param.data = param.data.clone()
-                            
+
         return clone_param
-    
+
     def get_updated_parameters(self):
         return self.local_weight_updated
-    
+
     def update_parameters(self, new_params):
-        for param , new_param in zip(self.model.parameters(), new_params):
+        for param, new_param in zip(self.model.parameters(), new_params):
             param.data = new_param.data.clone()
             param.grad.data = new_param.grad.data.clone()
 
     def get_grads(self, grads):
 
         self.optimizer.zero_grad()
-        
+
         for x, y in self.trainloaderfull:
             output = self.model(x)
             loss = self.loss(output, y)
@@ -100,12 +103,12 @@ class Edgebase:
             output = self.model(x)
             train_acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
             loss += self.loss(output, y)
-        return train_acc, loss , self.train_samples
-    
+        return train_acc, loss, self.train_samples
+
     def update_direction(self):
-        #self.model.parameters() is model updated from server
-        for opitmal_param, server_param, dt in zip(self.local_optimal,self.model.parameters(), self.dt):
-            dt.data = 1/self.eta * (opitmal_param.data.clone() - server_param.data.clone())
+        # self.model.parameters() is model updated from server
+        for opitmal_param, server_param, dt in zip(self.local_optimal, self.model.parameters(), self.dt):
+            dt.data = 1 / self.eta * (opitmal_param.data.clone() - server_param.data.clone())
 
     def get_next_train_batch(self):
         try:
@@ -116,7 +119,7 @@ class Edgebase:
             self.iter_trainloader = iter(self.trainloader)
             (X, y) = next(self.iter_trainloader)
         return (X, y)
-    
+
     def get_next_test_batch(self):
         try:
             # Samples a new batch for persionalizing
@@ -136,7 +139,30 @@ class Edgebase:
     def load_model(self):
         model_path = os.path.join("models", self.dataset)
         self.model = torch.load(os.path.join(model_path, "server" + ".pt"))
-    
+
     @staticmethod
     def model_exists():
         return os.path.exists(os.path.join("models", "server" + ".pt"))
+
+    def regularize(self):
+        reg = 0
+        for param in self.model.parameters():
+            reg += param.norm() ** 2
+        return (self.L / 2) * reg
+
+    def total_loss(self, model=None, X=None, y=None, full_batch=True, regularize=False):
+        if model is None:
+            model = self.model
+        if X is None:
+            if not full_batch:
+                X, y = self.get_next_train_batch()
+        loss = 0.
+        if full_batch:
+            for X, y in self.trainloaderfull:
+                loss += self.loss(model(X), y)
+        else:
+            loss = self.loss(model(X), y)
+        if regularize:
+            loss += self.regularize()
+        return loss
+    
