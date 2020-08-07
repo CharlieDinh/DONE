896f0a55eaeaa55fb0dcb1a7b6c66a72c6805c9f
CharlieDinh <CharlieSex@156>
2020-08-07 13:31:05 +1000

    update plot

--- main_plot_mnist.py
+++ main_plot_mnist.py
@@ -48,3 +48,14 @@ if(0):
     L = [0,0,0,0,0,0,0,0,0,0,0,0]
     #kappa = [4,4,4,4,4,4,9,9,9,9,9,9]
     plot_summary_mnist_R_and_alpha(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, alpha = alpha, eta = eta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
+
+if(1):
+    numedges = [8,16,24,32]
+    local_epochs = [20,50,80,120]
+    learning_rate = [1,1,1,1]
+    alpha =  [0.015,0.015,0.015,0.015]
+    eta = [1,1,1,1]
+    batch_size = [0,0,0,0]
+    algorithms = ["DONE","DONE","DONE","DONE"]
+    L = [0,0,0,0]
+    plot_summary_mnist_edge(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, alpha = alpha, eta = eta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
