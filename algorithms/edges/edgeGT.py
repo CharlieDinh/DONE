6d994df453a8589fc43aa28531a1b09322ea10f0
unknown <amirrezaeibalef@gmail.com>
2020-10-21 04:25:47 +0330

    flat structure is removed

--- algorithms/edges/edgeGT.py
+++ algorithms/edges/edgeGT.py
@@ -8,6 +8,7 @@ import json
 from torch.utils.data import DataLoader
 from algorithms.edges.edgebase import Edgebase
 from algorithms.optimizers.optimizer import *
+from algorithms.edges.nn_utils import dot_product,add_params
 
 # Implementation for Conjugate gradient method clients
 
@@ -50,52 +51,40 @@ class edgeGT(Edgebase):
         loss.backward(create_graph=True)
         
         
-        for param, d in zip(self.model.parameters(), self.dt):
+        tol = 1e-8 # threshold for stopping cg iterations
+        r_vec =[]
+        p_vec =[]
+        for param, d, grad in zip(self.model.parameters(), self.dt,self.server_grad):
             # Set direction to 0 at the begining
             d.data = - 0 * param.grad.data.clone()
+            r_vec.append(-grad.grad.detach().clone())
+            p_vec.append(-grad.grad.detach().clone())
         
-        # matrices should be flatten
-        grads = torch.cat([x.grad.data.view(-1) for x in self.server_grad]).reshape(-1,1)
-        dt = torch.cat([x.data.view(-1) for x in self.dt]).reshape(-1,1)
-
         # conjugate gradien initials 
-        tol = 1e-8 # threshold for stopping cg iterations
-        r = -grads #+ torch.mm(hess,dt) # direction is 0 at the begining
-        p = r.clone().detach()
-        rsold = torch.dot(r.view(-1), r.view(-1))
+        rsold = dot_product(r_vec, r_vec)
         
         # conjugate gradien iteration
         for i in range(1, self.local_epochs + 1):  # R
-            index=0
-            p_vec=[]
-            for d in self.dt:
-                shape = d.data.shape
-                p_vec.append(p[index: index+ d.data.numel()].reshape(shape))
-                index = index+ d.data.numel()
-            hess_vec_prods = self.hessian_vec_prod(loss, list(self.model.parameters()), p_vec)
-            hess_p = torch.cat([x.data.view(-1) for x in hess_vec_prods]).reshape(-1,1)
-            hess_p = hess_p + self.alpha * p
-            alpha  = rsold /torch.dot(p.view(-1),hess_p.view(-1))
-            dt.data = dt.data + alpha * p
-            r.data = r - alpha* hess_p
-            rsnew = torch.dot(r.view(-1), r.view(-1))
+            hess_p = self.hessian_vec_prod(loss, list(self.model.parameters()),p_vec)
+            add_params(hess_p,p_vec,ratio=self.alpha)
+
+            alpha  = rsold /dot_product(p_vec,hess_p)
+            
+            add_params(self.dt,p_vec,ratio=alpha)
+            add_params(r_vec,hess_p,ratio=-alpha)
+            
+            rsnew = dot_product(r_vec, r_vec)
             if np.sqrt(rsnew.detach().numpy()) < tol:
                 #print('Itr:', i)
                 break
             else:
-                p.data = r + (rsnew / rsold)* p
-                rsold.data = rsnew
-        
-        # coppying rsult to self.dt
-        index=0
-        for d in self.dt:
-            shape = d.data.shape
-            d.data = dt[index: index+ d.data.numel()].reshape(shape)
-            index = index+ d.data.numel()
-          
+                p_vec = add_params(r_vec,p_vec,ratio=(rsnew / rsold),in_place=False)
+                rsold = rsnew
+
     def hessian_vec_prod(self, loss, params, dt):
         self.model.zero_grad()
         grads = torch.autograd.grad(loss, params, create_graph=True, retain_graph=True)
         self.model.zero_grad()
         hv = torch.autograd.grad(grads, params, dt, only_inputs=True, create_graph=True, retain_graph=True)
-        return hv    
+        return hv
+          
