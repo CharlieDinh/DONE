3a300ad63d931626d4c6e5b4abf197c5deea1661
CharlieDinh <thecanhdinh92@gmail.com>
2020-12-30 15:06:23 +1100

    update plot

--- main_plot_mnist.py
+++ main_plot_mnist.py
@@ -25,10 +25,11 @@ torch.manual_seed(0)
     
 numedges = 32
 num_glob_iters = 100
-dataset = "MNIST"
-
+#dataset = "Mnist"
+dataset = "Nist"
+#dataset = "human_activity"
 # defind parameters
-if(1):
+if(0):
     numedges =[32,32,32,32,32,32,32,32]
     local_epochs = [120,190,200,120,120,20,1]
     learning_rate = [1,1,1,1,1,0.05,0.2]
@@ -41,17 +42,42 @@ if(1):
 
 if(0):
     numedges = [32, 32, 32, 32, 32, 32, 32, 32, 32, 32]
-    local_epochs = [20,50,80,100,100,100,100,100]
+    local_epochs = [10,20,30,40,40,40,40,40]
     learning_rate = [1,1,1,1,1,1,1,1]
-    alpha =  [0.02,0.02,0.02,0.02,0.005,0.01,0.02,0.03]
-    eta = [1,1,1,1,1,1,1,1,1,1,1,1]
+    alpha =  [0.03,0.03,0.03,0.03,0.005,0.01,0.02,0.03]
+    eta = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
     batch_size = [0,0,0,0,0,0,0,0,0,0,0,0]
     algorithms = ["DONE","DONE", "DONE", "DONE", "DONE", "DONE", "DONE", "DONE"]
     L = [0,0,0,0,0,0,0,0,0,0,0,0]
     #kappa = [4,4,4,4,4,4,9,9,9,9,9,9]
     plot_summary_mnist_R_and_alpha(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, alpha = alpha, eta = eta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
 
+if(0):
+    numedges = [30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
+    local_epochs = [10,20,30,40,40,40,40,40]
+    learning_rate = [1,1,1,1,1,1,1,1]
+    alpha =  [0.02,0.02,0.02,0.02,0.006,0.008,0.01,0.02]
+    eta = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    batch_size = [0,0,0,0,0,0,0,0,0,0,0,0]
+    algorithms = ["DONE","DONE", "DONE", "DONE", "DONE", "DONE", "DONE", "DONE"]
+    L = [0,0,0,0,0,0,0,0,0,0,0,0]
+    #kappa = [4,4,4,4,4,4,9,9,9,9,9,9]
+    plot_summary_human_R_and_alpha(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, alpha = alpha, eta = eta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
+
+
 if(1):
+    numedges = [32, 32, 32, 32, 32, 32, 32, 32, 32, 32]
+    local_epochs = [10,20,30,40,40,40,40,40]
+    learning_rate = [1,1,1,1,1,1,1,1]
+    alpha =  [0.01,0.01,0.01,0.01,0.004,0.006,0.008,0.01]
+    eta = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    batch_size = [0,0,0,0,0,0,0,0,0,0,0,0]
+    algorithms = ["DONE","DONE", "DONE", "DONE", "DONE", "DONE", "DONE", "DONE"]
+    L = [0,0,0,0,0,0,0,0,0,0,0,0]
+    plot_summary_nist_R_and_alpha(num_users=numedges, loc_ep1=local_epochs, Numb_Glob_Iters=num_glob_iters, lamb=L, learning_rate=learning_rate, alpha = alpha, eta = eta, algorithms_list=algorithms, batch_size=batch_size, dataset=dataset)
+
+
+if(0):
     
     numedges = [8,16,24,32]
     local_epochs = [20,50,80,120]
